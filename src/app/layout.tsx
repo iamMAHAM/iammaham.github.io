c9c17362d1cbe4eda62677e79efa32abdf38ae25
iamMAHAM <abdulkabore@gmail.com>
2023-05-01 16:12:28 +0000

    add metadata type"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'iamMaham',
   description: "iamMAHAM's portefolio website",
 };
